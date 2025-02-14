9d94981b81b7b37393f781c02ec2f94dc423f012
Felix Hagemans <felixhagemans@gmail.com>
2025-02-14 13:00:07 +0100

    Support for configuring default days

--- src/main/kotlin/nl/codeclan/timesheet/entities/Location.kt
+++ src/main/kotlin/nl/codeclan/timesheet/entities/Location.kt
@@ -3,6 +3,7 @@ package nl.codeclan.timesheet.entities
 import ValidLocation
 import jakarta.persistence.*
 import jakarta.validation.constraints.NotBlank
+import java.time.DayOfWeek
 
 @Entity
 @ValidLocation
@@ -23,6 +24,12 @@ class Location(
 
     @Column
     var distance: Int? = null,
+
+    @ElementCollection(targetClass = DayOfWeek::class, fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "default_days", joinColumns = [JoinColumn(name = "location_id")])
+    @Column(name = "default_day")
+    var defaultDays: MutableList<DayOfWeek> = mutableListOf()
 ) {
     companion object {
         const val HOME = "Thuis"
--- src/main/kotlin/nl/codeclan/timesheet/service/TimesheetService.kt
+++ src/main/kotlin/nl/codeclan/timesheet/service/TimesheetService.kt
@@ -3,10 +3,11 @@ package nl.codeclan.timesheet.service
 import de.focus_shift.jollyday.core.HolidayManager
 import de.focus_shift.jollyday.core.ManagerParameters
 import nl.codeclan.timesheet.model.Day
-import nl.codeclan.timesheet.repository.findByMonth
 import nl.codeclan.timesheet.model.DayType
 import nl.codeclan.timesheet.model.Timesheet
 import nl.codeclan.timesheet.repository.DayRepository
+import nl.codeclan.timesheet.repository.LocationRepository
+import nl.codeclan.timesheet.repository.findByMonth
 import org.springframework.stereotype.Service
 import java.time.DayOfWeek
 import java.time.LocalDate
@@ -19,7 +20,10 @@ private val CLANDAY = LocalDate.of(2024, Month.JANUARY, 5)
 private val HOLIDAY_MANAGER = HolidayManager.getInstance(ManagerParameters.create(Locale.of("nl")))
 
 @Service
-class TimesheetService(val dayRepository: DayRepository) {
+class TimesheetService(
+    val dayRepository: DayRepository,
+    val locRepository: LocationRepository,
+) {
     fun generate(month: YearMonth): Timesheet {
         val types = determineTypes(month)
         return Timesheet(month, types)
@@ -27,15 +31,15 @@ class TimesheetService(val dayRepository: DayRepository) {
 
     private fun determineTypes(month: YearMonth): ArrayList<Day> {
         val persisted = dayRepository.findByMonth(month)
+        val locations = locRepository.findAll().flatMap { d -> d.defaultDays.map { it to d } }.toMap()
         var from = month.atDay(1)
         val until = month.atEndOfMonth()
         val days = ArrayList<Day>()
         val vacation = emptySet<LocalDate>()
         while (from <= until) {
-            //determineLocation(from, type)
             days.add(persisted[from]
                 ?.let { Day(it.type!!, it.location?.id) }
-                ?: Day(determineType(from, vacation), null))
+                ?: Day(determineType(from, vacation), locations[from.dayOfWeek]?.id))
             from = from.plusDays(1)
         }
         return days
@@ -58,19 +62,4 @@ class TimesheetService(val dayRepository: DayRepository) {
     private fun isClanday(day: LocalDate): Boolean {
         return day.dayOfWeek == DayOfWeek.FRIDAY && ChronoUnit.WEEKS.between(CLANDAY, day) % 2 == 0L
     }
-
-// TODO: Default day for location
-//    private fun determineLocation(day: LocalDate, type: DayType): Location? {
-//        return when(type) {
-//            DayType.WORK, DayType.CLANDAY -> {
-//                return when (day.dayOfWeek) {
-//                    DayOfWeek.TUESDAY -> Location.DUIVEN
-//                    DayOfWeek.WEDNESDAY -> Location.ARNHEM
-//                    DayOfWeek.FRIDAY -> Location.DENBOSCH
-//                    else -> Location.HOME
-//                }
-//            }
-//            else -> null
-//        }
-//    }
 }
